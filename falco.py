480d7152062372e503475ef4e079f8565ba98772
Alejandro Keller <alejandro.keller@fhnw.ch>
2023-03-08 15:07:44 +0100

    Update falco.py

--- falco.py
+++ falco.py
@@ -44,16 +44,24 @@ class Falco( minimalmodbus.Instrument ):
         return self.read_register(1003, 1)
 
     def get_unit(self):
-        """Return the measurement unit. char ['p' or 'g'], 1 Register"""
-        return self.read_string(1005, 1, number_of_registers = 1)
+        """Return the measurement unit. char ['p' or 'g'], 1 Register
+        
+        This translates to p = ppm or g = mg/m3.
+        """
+        u = self.read_string(1005, 1, number_of_registers = 1)
+        match u:
+        case 'p':
+            return 'ppm'
+        case 'g':
+            return 'mg/m3'
+        case _:
+            return 'ppm'   # ppm is the default case if u is not found
+
+        # return self.read_string(1005, 1, number_of_registers = 1)
 
     def get_rf(self):
         """Return the response factor. 32 bit float [0.1 - 10.0], 2 Registers"""
         return self.read_float(1010, 1)
-
-    def get_range(self):
-        """Return the sensor range. 16 bit unsignet int [10, 50, 1000, 3000], 1 Register"""
-        return self.read_register(1012, 1)
 
     def get_range(self):
         """Return the sensor range. 16 bit unsignet int [10, 50, 1000, 3000], 1 Register"""
@@ -66,6 +74,47 @@ class Falco( minimalmodbus.Instrument ):
     def get_cal3000(self):
         """Return the cal 3000 value. 16 bit unsignet int [0 - 65535 (Default value 3000) ], 1 Register"""
         return self.read_register(1061, 1)
+
+    def readline(self):
+        """Return a list of dictionaries with current data.
+
+        dictionary keys are:
+            'var' -> variable name string
+            'val' -> value
+            'unit' -> unit string when applicable, otherwise '-'
+        """
+        response = [
+            {'var': 'VOC',
+            'val': self.get_voc(),
+            'unit': '-'},
+            {'var': 'Unit',
+            'val': self.get_unit(),
+            'unit': '-'},
+            {'var': 'Voltage',
+            'val': self.get_voltage(),
+            'unit': 'mV'},
+            {'var': 'T',
+            'val': self.get_temperature(),
+            'unit': 'degC'},
+            {'var': 'RF',
+            'val': self.get_rf(),
+            'unit': '-'},
+            {'var': 'Range',
+            'val': self.get_range(),
+            'unit': '-'},
+            {'var': 'Cal100',
+            'val': self.get_cal100(),
+            'unit': '-'},
+            {'var': 'Cal3000',
+            'val': self.get_cal3000(),
+            'unit': '-'}
+        ]
+
+        return response
+
+
+
+        
 
     
 

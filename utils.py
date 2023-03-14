620ffd1dde28c33a69123045547b0ea63cc04b51
Alejandro Keller <alejandro.keller@fhnw.ch>
2023-03-14 07:36:35 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import pyqtgraph as pg
 import datetime
 import time
-
+import sys
+import socket
 
 def timestamp():
     return int(time.mktime(datetime.datetime.now().timetuple()))
@@ -15,3 +16,31 @@ class TimeAxisItem(pg.AxisItem):
 
     def tickStrings(self, values, scale, spacing):
         return [datetime.datetime.fromtimestamp(value).strftime("%H:%M") for value in values]
+
+def log_message(module, msg):
+        """
+        Logs a message with standard format
+        """
+        timestamp = time.strftime("%Y.%m.%d-%H:%M:%S ")
+        log_message = "- [{0}] :: {1}"
+        log_message = timestamp + log_message.format(module,msg)
+        print(log_message, file=sys.stderr)
+
+def send_string(line, server_address, sock = 0):
+    # Sends a string to through a TCP socket
+
+    # Send data
+    try:
+        if not sock:
+            # Create a TCP/IP socket
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(server_address)
+            
+        sock.sendall(line.encode())
+    except socket.error:
+#        print("nobody listening", file = sys.stderr)
+        sock.close()
+#        print('closing socket', file = sys.stderr)
+        sock = 0
+
+    return sock
